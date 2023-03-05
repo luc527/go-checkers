820a590eb9b20a0a7a4c3ab805a18d21e37417a6
Lucas Moraes Schwambach <lucas.schwambach52@gmail.com>
2023-03-05 17:39:05 -0300

    remove TODO

--- main.go
+++ main.go
@@ -1,28 +1,6 @@
 package main
 
-import "fmt"
-
 // TODO the generateXxx procedures should take a color, duh
 
 func main() {
-
-	// TODO make some proper unit tests for this
-
-	b := new(board)
-
-	b.set(2, 2, whiteColor, pawnKind)
-	b.set(3, 3, blackColor, pawnKind)
-
-	fmt.Println("before:")
-	fmt.Println(b)
-
-	var iss [][]instruction
-	iss = generateCaptureMoves(iss, b)
-
-	fmt.Println("after (should be the same):")
-	fmt.Println(b)
-	fmt.Printf("instructions (%d):\n", len(iss))
-	for _, is := range iss {
-		fmt.Println(is)
-	}
 }
