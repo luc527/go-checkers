6ae75d419d77b9f1d2c6543ddd515a77577c9641
Lucas Moraes Schwambach <lucas.schwambach52@gmail.com>
2023-03-19 14:20:32 -0300

    some profiling

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"log"
+	"math/rand"
+	"os"
+	"runtime/pprof"
+)
+
 func main() {
-	play()
+	f, perr := os.Create("cpu.pprof")
+	if perr != nil {
+		log.Fatal(perr)
+	}
+	pprof.StartCPUProfile(f)
+	defer pprof.StopCPUProfile()
+
+	trials := 100_000
+	for t := 0; t < trials; t++ {
+		g := NewStandardGame(CapturesMandatory, BestNotMandatory)
+		for !g.IsOver() {
+			plies := g.Plies()
+			random := plies[rand.Int()%len(plies)]
+			g.DoPly(random)
+		}
+		for g.HasLastPly() {
+			g.UndoLastPly()
+		}
+	}
 }
